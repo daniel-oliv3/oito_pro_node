6718b03c93dc322d43db1c333275bb67e9d238df
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2021-08-26 15:55:40 -0300

    Formulario Login

--- login_front/src/pages/Login/index.js
+++ login_front/src/pages/Login/index.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const Login = () => {
+
+    const [dadosUsuario, SetUsuario] = useState({
+        usuario: '',
+        senha: ''
+    });
+
+    const valorInput = e => SetUsuario({ ...dadosUsuario, [e.target.name]: e.target.value});
+
+    const loginSubmit = async e => {
+        e.preventDefault();
+        console.log(dadosUsuario.usuario);
+        console.log(dadosUsuario.senha);
+    };
+
     return (
         <div>
             <h1>Login</h1>
+
+            <form onSubmit={loginSubmit}>
+                <label>Usuário: </label>
+                <input type="text" name="usuario" placeholder="Digite o usuário" onChange={valorInput} /><br /><br />
+
+                <label>Usuário: </label>
+                <input type="password" name="senha" placeholder="Digite a senha" onChange={valorInput} /><br /><br />
+
+                <button type="submit">Acessar</button>
+            </form>
         </div>
     );
 }
