86b8e008769903ef569a90cae127cd84b7ff1c4a
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2021-08-25 22:09:59 -0300

    Listar

--- api_n/app.js
+++ api_n/app.js
@@ -19,10 +19,17 @@ app.use((req, res, next) => {
 });
 
 
-app.get('/usuarios', eAdmin, function (req, res){
-    return res.json({
-        erro: false,
-        messagem: "Listar usuários!"
+app.get('/usuarios', eAdmin, async function (req, res){
+    await Usuario.findAll({order: [['id', 'DESC']]}).then(function(usuarios){
+        return res.json({
+            erro: false,
+            usuarios 
+        });
+    }).catch(function(){
+        return res.json({
+            erro: true,
+            messagem: "Erro: Nenhum usuário encontrado!"
+        });
     });
 });
 
